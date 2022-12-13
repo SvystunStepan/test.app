caa4a29c44b2f734be9f009ae9adcdf5d5349a37
SvystunStepan <s913ua@gmail.com>
2022-12-13 21:09:50 +0200

    13.12

--- JavaScript homework2/script/app.js
+++ JavaScript homework2/script/app.js
@@ -1,69 +1,94 @@
 // Домашнє завдання:
-// 2.=========================================
+// 3-4.=========================================
+/* Создать объект с методами, которые будут динамически генерировать DOM вот такой страницы.
+Это будет тест, который мы будем разрабатывать в следующих заданиях.
+Сейчас вам нужно только динамически создать html, методы должны храниться в одном объекте.
+Для того чтоб страница выглядела красиво можете использовать Bootstrap и создавать DOM-элементы с готовыми бутстраповскими классами.
+Изначально на странице должен быть только <body>, вызывая методы объекта нужно создать dom-элементы */
 
-let test = document.querySelector('#test');
-test.innerHTML = 'Тест по програмуванню';
-
+let div = document.createElement('div');
+div.className = "test";
+div.innerHTML = "<h2>Тест по програмуванню</h2>";
+document.body.append(div);
 
 let questions = [
   {
     text: 'Питання №1',
-    answers: [      'Варіант відповіді №1', 
-              '<br/> Варіант відповіді №2', 
-              '<br/> Варіант відповіді №3'],
+    answers: [ 
+              'Варіант відповіді №1', 
+              'Варіант відповіді №2', 
+              'Варіант відповіді №3'
+            ],
     right: 0,
   },
   {
     text: 'Питання №2',
-    answers: [      'Варіант відповіді №1', 
-              '<br/> Варіант відповіді №2', 
-              '<br/> Варіант відповіді №3'],
+    answers: [ 
+              'Варіант відповіді №1', 
+              'Варіант відповіді №2', 
+              'Варіант відповіді №3'
+            ],
     right: 1,
   },
   {
     text: 'Питання №3',
-    answers: [      'Варіант відповіді №1', 
-              '<br/> Варіант відповіді №2', 
-              '<br/> Варіант відповіді №3'],
+    answers: [
+              'Варіант відповіді №1', 
+              'Варіант відповіді №2', 
+              'Варіант відповіді №3'
+            ],
     right: 2,
   },
 ];
 
 
+for (let i = 0; i < questions.length; i++) {
+  let form = document.createElement('form');
+  div.appendChild(form);
+  //form.dataset.right = questions.right; //
 
-let i = 1;
-for (let question of questions) {
-  let div = document.createElement('div');
-  test.appendChild(div);
+  let h4 = document.createElement('h4');
+  h4.innerHTML = questions[i].text;
+  form.appendChild(h4);
 
   let ul = document.createElement('ul');
-  ul.innerHTML = question.text;
-  div.appendChild(ul);
 
-  let li = document.createElement('li');
-  li.innerHTML = question.answers;
-  ul.appendChild(li);
+  for (let j = 0; j < questions[i].answers.length; j++) {
+     let li = document.createElement('li');
+     li.innerText = questions[i].answers[j];
 
-  let form = document.createElement('form');
-  li.appendChild(form);
-  form.dataset.right = question.right;
+     /*добавити label>input + text*/
+    let label = document.createElement('label')//
+    //label.innerText = questions[i].answers[j];
+    
+      let input = document.createElement('input')
+      input.setAttribute('value', j)
+      input.setAttribute('type', 'radio')
+      input.setAttribute('name', 'answers'+i)
 
-  let j = 0;
-  for (let answer of question.answers) {
-      let input = document.createElement('input');
-      input.type = 'radio'; //checkbox
-      input.name = i;
-      input.dataset.answerNum = j++;
-      form.append(input); 
+    label.appendChild(input); //
+
+    li.prepend(label); //li.prepend(input);
+
+   ul.appendChild(li); //ul.appendChild(li);
   }
-
-i++;
-
+  form.appendChild(ul)
 }
 
-let button = document.querySelector('#button');
+let button = document.createElement('button');
+button.innerHTML = "Перевірка";
+div.append(button);
+
+//console.log(questions[0].right); //(як вивести правильну відповідь в консоль)
+
+
+
+
+/* //==== перевірка на правильність (не фуричить) =====
+
+// let button = document.querySelector('#button');
 button.addEventListener('click', function() {
-  let forms = document.querySelectorAll('#test form');
+  let forms = document.querySelectorAll('form'); //('div form')
 
   for (let form of forms) {
     form.classList.remove('correct');
@@ -73,7 +98,7 @@ button.addEventListener('click', function() {
 
     for (let input of inputs) {
       if (input.checked) {
-        if (input.dataset.answerNum == form.dataset.right) {
+        if (input.value == questions[0]['right']) { // if (input.dataset.answerNum == form.dataset.right) {
         form.classList.add('correct');
       } else {
         form.classList.add('incorrect');
@@ -83,71 +108,7 @@ button.addEventListener('click', function() {
       }
     }
   }
-});
-
-//====================================================
-
-
-
-
-
-// let div = document.createElement('div');
-// div.className = "test";
-// div.innerHTML = "Тест по програмуванню";
-
-// document.body.append(div);
-
-
-
-/* let ol = document.createElement('ol');
-// ol.className = "test-item";
-div.append(ol); */
-
-// test.append(getListContent());
-
-
-
-
-
-
-/* 
-var element = document.createElement('div');
-element.classList.add ('');
-element.innerHTML='Тест по програмуванню';
-
-var body = document.querySelector('.body');
-// body.appendChild(element);
-body.insertBefore(element, body.children[1]); */
-
-
-
-
-/* let div = document.createElement('div');
-div.className = "test";
-div.innerHTML = "Тест по програмуванню";
-
-document.body.append(div);
-
-let ol = document.createElement('ol');
-ol.className = "test-item";
-
-div.appendChild(ol);
-
-
-function getListContent() {
-  let fragment = new DocumentFragment();
-
-  for(let i=1; i<=3; i++) {
-    let li = document.createElement('li');
-    li.append('Питання №', i);
-    fragment.append(li);
-  }
-  
-  return fragment;
-}
-
-ol.append(getListContent()); */
-
+}) */
 
 
 
